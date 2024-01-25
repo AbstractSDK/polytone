1f130ebb2fdeb34cee00845180e770d4327b06f2
Kayanski <44806566+Kayanski@users.noreply.github.com>
2024-01-25 16:48:36 +0100

    Deploy (#3)
    * Added cw-orch + Deploy trait
    
    * Reverted cw-multi-test
    
    * Revert format
    
    * Removed unused deps
    
    * Removed unused deps
    
    * Format
    
    * Changed contract dependency to workspace
    
    * Added more state
    
    * Add github action for wasm build
    
    * test ci on push
    
    * use my credentials for pushing
    
    * Update artifacts [skip ci]
    
    * only run on push to main
    
    * Uploaded contracts
    
    * Corrected interface + state file
    
    * Better scripts + docs
    
    * Nits
    
    * Added juno and terra
    
    * Bump cw-orch
    
    * Bumped versioning
    
    * Updated cosmwasm
    
    * Added stargaze and neutron
    
    * Updated table
    
    * Format table
    
    * Added deployments, + changed cw-rch-state file
    
    * Renames and gitignore
    
    * Changed CI config
    
    * Updated cw-orch
    
    * Added scripts to clear admins
    
    * Added Kujira Connections
    
    * Cleanup
    
    * Removed artifacts
    
    * Added query channel scripts
    
    * Start for merging with main
    
    ---------
    
    Co-authored-by: cyberhoward <cyberhoward@protonmail.com>
    Co-authored-by: CyberHoward <CyberHoward@users.noreply.github.com>

---
+++ scripts/src/bin/check_admin.rs
@@ -0,0 +1,63 @@
+use anyhow::bail;
+use cw_orch::daemon::networks::{JUNO_1, NEUTRON_1, OSMOSIS_1, PHOENIX_1};
+use cw_orch::daemon::queriers::{CosmWasm, DaemonQuerier};
+use cw_orch::daemon::Daemon;
+use cw_orch::state::{ChainState, StateInterface};
+use cw_orch::{
+    daemon::{networks::ARCHWAY_1, ChainInfo, DaemonBuilder},
+    tokio::runtime::Runtime,
+};
+use scripts::helpers::get_deployment_id;
+use scripts::STARGAZE_1;
+
+pub fn main() {
+    dotenv::dotenv().unwrap();
+    pretty_env_logger::init();
+    let mut juno = JUNO_1;
+    juno.grpc_urls = &["http://juno-grpc.polkachu.com:12690"];
+    let all_chains = &[juno, OSMOSIS_1, NEUTRON_1, STARGAZE_1, PHOENIX_1];
+    for chain in all_chains {
+        check_admins(&ARCHWAY_1, chain).unwrap();
+        check_admins(chain, &ARCHWAY_1).unwrap();
+    }
+}
+
+pub fn check_admins(src_chain: &ChainInfo, dst_chain: &ChainInfo) -> anyhow::Result<()> {
+    let rt = Runtime::new()?;
+    let deployment_id = get_deployment_id(src_chain, dst_chain);
+    let src_daemon = DaemonBuilder::default()
+        .handle(rt.handle())
+        .deployment_id(deployment_id.clone())
+        .chain(src_chain.clone())
+        .build()?;
+    let dst_daemon = DaemonBuilder::default()
+        .handle(rt.handle())
+        .deployment_id(deployment_id.clone())
+        .chain(dst_chain.clone())
+        .build()?;
+
+    check_one_chain_admin(&src_daemon)?;
+    check_one_chain_admin(&dst_daemon)?;
+
+    Ok(())
+}
+
+pub fn check_one_chain_admin(daemon: &Daemon) -> anyhow::Result<()> {
+    let addresses = daemon.state().get_all_addresses()?;
+
+    let wasm = CosmWasm::new(daemon.channel());
+
+    addresses
+        .values()
+        .map(|a| {
+            let contract_info = daemon.rt_handle.block_on(wasm.contract_info(a))?;
+            if !contract_info.admin.is_empty() {
+                bail!("{:?} admin is non empty", a);
+            }
+
+            Ok(())
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    Ok(())
+}
